5d4f5c632ffb15a4fbaa9e0bbcc268af08ffc3f1
yi-xuan-97 <yfeng@pdx.edu>
2020-06-02 10:58:41 -0700

    appending chekcbox

--- todo.js
+++ todo.js
@@ -1,12 +1,28 @@
+var i;
 function append() {
 
   var todo = document.getElementById("alist").value;
 
   var final = document.createElement("li");
-  final.style= "list-style:none; text-align:center; font-size:22px;";
+
+  final.style = "list-style:none; text-align:center; font-size:22px;";
+  final.id = "als";
+
   final.innerHTML = todo;
+  // final.innerHTML = todo + "<input type='checkbox'  id='abut'>";
+
   document.getElementById("aform").appendChild(final);
 
-  document.getElementById("alist").value='';
+  var but =document.createElement("input");
 
+  but.type = "checkbox";
+  but.id="but";
+
+  document.getElementById("aform").appendChild(final);
+  
+
+
+  document.getElementById("alist").value = '';
 }
+
+
