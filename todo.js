96f8cf6e24af9b75d15d7a753ea3b5bb468feef2
yi-xuan-97 <yfeng@pdx.edu>
2020-06-02 11:45:46 -0700

    make label connected to checkbox

--- todo.js
+++ todo.js
@@ -1,28 +1,26 @@
-var i;
-function append() {
+var sub = document.getElementById("list");
+// var timer = setInterval(helper, num*1000);
+var chec;
+
+sub.onclick = function () {
 
   var todo = document.getElementById("alist").value;
 
-  var final = document.createElement("li");
+  var final = document.createElement("label");
 
-  final.style = "list-style:none; text-align:center; font-size:22px;";
-  final.id = "als";
-
+  final.style = "display:block; padding-left:25px; font-size:22px;";
+  final.htmlFor = "but";
   final.innerHTML = todo;
-  // final.innerHTML = todo + "<input type='checkbox'  id='abut'>";
-
   document.getElementById("aform").appendChild(final);
 
-  var but =document.createElement("input");
+  chec = document.createElement("input");
+  chec.id = "but";
+  chec.type = "checkbox";
+  chec.style = "display:none";
+  document.getElementById("aform").appendChild(chec);
 
-  but.type = "checkbox";
-  but.id="but";
-
-  document.getElementById("aform").appendChild(final);
-  
-
+  var brea = document.createElement("br");
+  document.getElementById("aform").appendChild(brea);
 
   document.getElementById("alist").value = '';
 }
-
-
