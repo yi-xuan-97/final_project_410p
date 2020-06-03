f59ddfa5011f6050f3e8b7e55d3d6516f4cf95a0
yi-xuan-97 <yfeng@pdx.edu>
2020-06-03 16:01:31 -0700

    fixed chart can't use fetched data

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 var url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=677b08102882beabdba992496310ea0c';
 
-var curr=0;
+var curr = 0;
 let getDataPromise = (url) => {
 
   fetch(url)
@@ -9,6 +9,45 @@ let getDataPromise = (url) => {
       return response.json();
     })
     .then(data => {
+      var t1 = data.main.temp;
+      var t2 = data.main.feels_like;
+      var t3 = data.main.temp_min;
+      var t4 = data.main.temp_min;
+
+      var ctx = document.getElementById("myChart");
+      var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Temporature', 'Feel Like', 'Lowest Temp', 'Highest Temp', 'Mcomfort', 'mconfort'],
+          datasets: [{
+            label: 'Weather Infor',
+            barPercentage: 0.5,
+            barThickness: 4,
+            maxBarThickness: 8,
+            minBarLength: 2,
+            //data: [289.4, 285.4, 288.7, 290.3, 295.1, 300.1],
+            data: [t1, t2, t3, t4, 295.1, 300.1],
+            backgroundColor: [
+              'rgba(54, 162, 235, 0.8)',
+              'rgba(255, 206, 86, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(75, 192, 192, 0.8)',
+              'rgba(153, 102, 255, 0.8)',
+              'rgba(255, 159, 64, 0.8)',
+              // 'rgba(199, 199, 199, 0.8)',
+            ]
+          }]
+        },
+        options: {
+          scales: {
+            xAxes: [{
+              gridLines: {
+                offsetGridLines: true
+              }
+            }]
+          }
+        }
+      });
       console.log(data);
     })
     .catch(error => {
@@ -19,37 +58,5 @@ let getDataPromise = (url) => {
 
 getDataPromise(url);
 
-var ctx = document.getElementById("myChart");
-var myBarChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Temporature', 'Feel Like', 'Lowest Temp', 'Highest Temp', 'Mcomfort', 'mconfort'],
-    datasets: [{
-      label: 'Weather Infor',
-      barPercentage: 0.5,
-      barThickness: 4,
-      maxBarThickness: 8,
-      minBarLength: 2,
-      data: [289.4, 285.4, 288.7, 290.3, 295.1, 300.1],
-      backgroundColor: [
-        'rgba(54, 162, 235, 0.8)',
-        'rgba(255, 206, 86, 0.8)',
-        'rgba(255, 99, 132, 0.8)',
-        'rgba(75, 192, 192, 0.8)',
-        'rgba(153, 102, 255, 0.8)',
-        'rgba(255, 159, 64, 0.8)',
-        // 'rgba(199, 199, 199, 0.8)',
-      ]
-    }]
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        gridLines: {
-          offsetGridLines: true
-        }
-      }]
-    }
-  }
-});
+
 
