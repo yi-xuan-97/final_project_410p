cb1e76e1f596f59907a80ac16065b67a786acb8d
yi-xuan-97 <56617127+yi-xuan-97@users.noreply.github.com>
2021-11-30 16:44:51 -0800

    Fixes #14

--- todolist.js
+++ todolist.js
@@ -29,26 +29,32 @@ list.addEventListener('click', function(curr) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
+
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var text = document.createTextNode(inputValue);
     li.appendChild(text);
+
     if (inputValue === '') {
         alert("You must write something!");
     } 
     else {
         document.getElementById("myUL").appendChild(li);
     }
+
     document.getElementById("myInput").value = "";
+
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
+
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
         }
     }
+
 }
