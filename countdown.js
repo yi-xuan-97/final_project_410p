dbe423c5eb1367ecf839732a63550f9d956b0732
DFedron <72382514+DFedron@users.noreply.github.com>
2021-12-03 16:03:03 -0800

    delete the variable leftTime, it's redundant.

--- countdown.js
+++ countdown.js
@@ -3,9 +3,8 @@ function countdown ()
     var curTime = new Date();
     //---------change the date in here---------
     var endTime = new Date("2020,6,13");
-    var leftTime = LeftTime();
 
-    document.getElementById("countdown").innerHTML = leftTime;
+    document.getElementById("countdown").innerHTML = LeftTime();
     
     function LeftTime() {
         return Math.ceil((endTime.getTime() - curTime.getTime()) / (24 * 60 * 60 * 1000));
